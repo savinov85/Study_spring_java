10747c4dab21b38fb533f23d6d339fd8241a3ae6
savinov85 <githubsavinov@mail.ru>
2019-10-11 09:03:36 +0800

    test

--- src/java/ru/savinov/springcourse/TestSpring.java
+++ src/java/ru/savinov/springcourse/TestSpring.java
@@ -5,9 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( );
 
         Music music = context.getBean("musicBean", Music.class); // Первый аргумент Id того бина который хотим получить, Второй аргумент название того класса бин которого получаем.
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
