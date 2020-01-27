d6868ce31cef5f059bd8aa2213b586e8d17b1d7a
Naglezh <savinov_vu@mail.ru>
2020-01-27 20:44:28 +0800

    Внедрение зависимостей. Внедрение переменных из файла в applicationContext

--- src/java/ru/savinov/springcourse/TestSpring.java
+++ src/java/ru/savinov/springcourse/TestSpring.java
@@ -5,7 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( );
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+          "applicationContext.xml"
+        );
 
         Music music = context.getBean("musicBean", Music.class); // Первый аргумент Id того бина который хотим получить, Второй аргумент название того класса бин которого получаем.
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
