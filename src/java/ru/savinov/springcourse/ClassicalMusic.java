c6974f3d56b003fe6c1c34ab161b76e42d78aded
Naglezh <savinov_vu@mail.ru>
2020-01-29 19:32:06 +0800

    factory-method.

--- src/java/ru/savinov/springcourse/ClassicalMusic.java
+++ src/java/ru/savinov/springcourse/ClassicalMusic.java
@@ -1,6 +1,11 @@
 package ru.savinov.springcourse;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){} //конструктор должен быть приватным для запрета создания объектов через new
+
+    public static ClassicalMusic getClassicalMusic(){  //фабричный метод по созданию объектов
+        return new ClassicalMusic(); //eсли у бина scope = "singleton" (по умолчанию) то объект по прежнему создастся один раз
+    }
     public void doMyInit() {
         System.out.println("Doing my initialization");// перед
     }
