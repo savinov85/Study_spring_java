6da89208bbbc6fb3098f32d62796975fd6f4e856
Naglezh <savinov_vu@mail.ru>
2020-01-29 19:17:23 +0800

    init-method. destroy-method. Создание, использование, удаление бинов с scope = "singleton" или "prototype"

--- src/java/ru/savinov/springcourse/ClassicalMusic.java
+++ src/java/ru/savinov/springcourse/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package ru.savinov.springcourse;
 
 public class ClassicalMusic implements Music{
+    public void doMyInit() {
+        System.out.println("Doing my initialization");// перед
+    }
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction!!");
+    }
     @Override
     public String getSong(){
         return "Hungarian Rhapsody";
--- src/java/ru/savinov/springcourse/TestSpring.java
+++ src/java/ru/savinov/springcourse/TestSpring.java
@@ -9,7 +9,14 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class); // Первый аргумент Id того бина который хотим получить, Второй аргумент название того класса бин которого получаем.
+        ClassicalMusic classicalMusic0 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic0.getSong());
+        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic1.getSong());
+        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic2.getSong());
+
+       /* Music music = context.getBean("musicBean", Music.class); // Первый аргумент Id того бина который хотим получить, Второй аргумент название того класса бин которого получаем.
         MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
@@ -23,7 +30,7 @@ public class TestSpring {
         firstMusicPlayer.setVolume("10"); // меняем звук у одной ссылки
 
         System.out.println("звук проигрывателя 1: " + firstMusicPlayer.getVolume());
-        System.out.println("звук проигрывателя 2: " + secondMusicPlayer.getVolume());
+        System.out.println("звук проигрывателя 2: " + secondMusicPlayer.getVolume());*/
         // звук меняется у объекта полученного по одной из ссылок следовательно объектов несколько
         // так работает scope = "prototype"
         context.close();
